1b8891a8f01a85fe7d757477c5c4f33c8ab6e5cf
Charlieee1 <nick@taberia.ca>
2021-06-08 17:17:16 -0400

    Tutorial-5

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -6,15 +6,16 @@ import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import renderEngine.RawModel;
 import renderEngine.Renderer;
+import shaders.StaticShader;
 
 public class MainGameLoop {
 
 	public static void main(String[] args) {
 
 		DisplayManager.createDisplay();
-
 		Loader loader = new Loader();
 		Renderer renderer = new Renderer();
+		StaticShader shader = new StaticShader();
 
 		float[] vertices = {
 				-0.5f, 0.5f, 0f,   // V0
@@ -22,23 +23,26 @@ public class MainGameLoop {
 				0.5f, -0.5f, 0f,   // V2
 				0.5f, 0.5f, 0f,    // V3
 		};
-		
+
 		int[] indices = {
 				0,1,3,  // Top left triangle (V0, V1, V3)
 				3,1,2   // Bottom right triangle (V3, V1, V2)
 		};
-		
+
 		RawModel model = loader.loadToVAO(vertices, indices);
 
 		while (!Display.isCloseRequested()) {
-			renderer.prepare();
 			// game logic
 			// render
+			renderer.prepare();
+			shader.start();
 			renderer.render(model);
+			shader.stop();
 			DisplayManager.updateDisplay();
 
 		}
 
+		shader.cleanUP();
 		loader.cleanUp();
 		DisplayManager.closeDisplay();
 
